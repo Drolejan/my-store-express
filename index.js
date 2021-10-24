a37816f3b973d6d71615bffe53b15a8ea1b304e9
drolejan <drolejan@gmail.com>
2021-10-24 00:29:51 -0500

    Day 4 Ready To Deploy

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 
 // traemos a express
 const express = require("express");
+const cors = require('cors');
 const routerApi = require("./routes");
 //const faker = require("faker");
+
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 // creamos una aplicación
 const app = express();
@@ -12,6 +15,18 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whitelist = ['http://localhost:3000', 'https://myapp.co','http://127.0.0.1:5500'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  }
+}
+app.use(cors(options));
 
 //definimos la ruta
 // tiene un callback que va a ejecutar la respuesta que enviemos al cliente.
@@ -26,7 +41,11 @@ app.get ("/nueva", (req, res) => {
 
   routerApi(app);
 
-//le decimos a la aplicación en que puesto escuchar
+  app.use(logErrors);
+  app.use(boomErrorHandler);
+  app.use(errorHandler);
+
+  //le decimos a la aplicación en que puesto escuchar
 // además creamos un callback que nos avisará cuando esté corriendo
 app.listen(port, () => {
   console.log("mi port" + port);
